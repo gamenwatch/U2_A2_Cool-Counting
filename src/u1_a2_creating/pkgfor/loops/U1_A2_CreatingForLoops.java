c5fd1b11dd6ffb63189974f163882acad31c0d84
jalac9997 <jalac9997@7KYM6Y1.Student.UGDSB.ED>
2018-10-19 10:20:52 -0400

    -created

---
+++ src/u1_a2_creating/pkgfor/loops/U1_A2_CreatingForLoops.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package u1_a2_creating.pkgfor.loops;
+
+import java.util.Scanner;
+
+/**
+ * Jacob L
+ * Oct 18, 2018
+ * Practice using for loops
+ */
+public class U1_A2_CreatingForLoops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Create Scanner
+        Scanner keyedInput = new Scanner(System.in);
+       
+        //variables
+        int firstChoice;
+        
+        // Introduction
+        System.out.println("---Cool Counting Program---\n");
+        System.out.println("Please enter your choice:");
+        System.out.println("1 . . . Count from 0 to 10 by 1");
+        System.out.println("2 . . . Count from 100 to 0 by 10");
+        System.out.println("3 . . . Count from 50 to 500 by 50");
+        System.out.println("4 . . . Count from 6000 to 1000 by 1000");
+        System.out.println("Please enter your choice:");
+        firstChoice = keyedInput.nextInt();
+        System.out.println("\n");
+        System.out.println("---------------------------------------------");
+        
+        switch (firstChoice) {
+            case 1:
+                
+        }
+        
+    }
+    
+}
